6cb4bc7f8d217ed90258eff869dc1b9c65132283
Vaidik Bhesaniya <144922727+vaidikbhesaniya@users.noreply.github.com>
2024-02-25 21:17:07 +0530

    Update controller.ts

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -613,25 +613,6 @@ export const deleteUser = async (req: Request, res: Response) => {
 	}
 };
 
-// DELETE : user/delete
-export const deleteUser = async (req: Request, res: Response) => {
-	const { email } = req.user;
-	const user = req.user as UserDocument;
-
-	try {
-		const deletedUser = await User.findOneAndDelete({ email });
-		await Transaction.deleteMany({ createdBy: user._id });
-		await History.deleteMany({ user: user._id });
-
-        if (!deletedUser) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        res.status(200).json({ message: "User deleted successfully" });
-    } catch (error) {
-        return res.status(500).json({ message: "Internal Server Error" });
-    }
-};
 
 // DELETE: /transaction/delete/:transactionId
 export const deleteTransaction = async (req: Request, res: Response) => {
