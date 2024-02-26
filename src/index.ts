ad28a3736fa8f44492c231477e33ddd6694f4888
Mr. V <vaidikbhesaniya1212@gmail.com>
2024-02-26 19:33:00 +0530

    add/creategroup

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,18 @@ import mongoose from "mongoose";
 import cron from "node-cron";
 import path from "path";
 // File Imports
-import { userRouter, transactionRouter } from "./routes/router";
+import { userRouter, transactionRouter, groupRouter } from "./routes/router";
 
 // Creating Backend Application
 const app: Express = express();
 
 // Middlewares
 app.use(
-	cors({
-		origin: "https://expenshare.vercel.app",
-		methods: ["GET", "POST", "PUT", "DELETE"],
-		credentials: true,
-	}),
+    cors({
+        origin: "https://expenshare.vercel.app",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
 );
 app.use(cookieParser());
 app.use(express.json());
@@ -30,31 +30,31 @@ app.set("views", path.resolve("./views"));
 // Routes
 app.use("/user", userRouter);
 app.use("/transaction", transactionRouter);
-
+app.use("/group", groupRouter);
 // Database Cleanup
 cron.schedule(
-  "0 * * * *",
-  async () => {
-    console.log("Running Database cleanup job...");
-    try {
-      const client = await MongoClient.connect(process.env.DB_URL!);
-      const db = client.db();
-      const otpCollection = db.collection("otps");
-      const userDataCollection = db.collection("userdatas");
-      const now = new Date();
+    "0 * * * *",
+    async () => {
+        console.log("Running Database cleanup job...");
+        try {
+            const client = await MongoClient.connect(process.env.DB_URL!);
+            const db = client.db();
+            const otpCollection = db.collection("otps");
+            const userDataCollection = db.collection("userdatas");
+            const now = new Date();
 
-      await otpCollection.deleteMany({ expires: { $lt: now } });
-      await userDataCollection.deleteMany({ expires: { $lt: now } });
-      console.log("Expired Colections cleared successfully.");
-      client.close();
-    } catch (error) {
-      console.error("Error clearing Expired Collection:", error);
+            await otpCollection.deleteMany({ expires: { $lt: now } });
+            await userDataCollection.deleteMany({ expires: { $lt: now } });
+            console.log("Expired Colections cleared successfully.");
+            client.close();
+        } catch (error) {
+            console.error("Error clearing Expired Collection:", error);
+        }
+    },
+    {
+        scheduled: true,
+        timezone: "Asia/Kolkata",
     }
-  },
-  {
-    scheduled: true,
-    timezone: "Asia/Kolkata",
-  }
 );
 
 // DB Connection
@@ -62,13 +62,13 @@ const PORT: number = 8080 | Number(process.env.PORT);
 const DB_URL: string = String(process.env.DB_URL);
 
 mongoose
-  .connect(DB_URL)
-  .then(() => {
-    console.log("Database Connected");
-    app.listen(PORT, () => {
-		console.log("Server Started");
-	});
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect(DB_URL)
+    .then(() => {
+        console.log("Database Connected");
+        app.listen(PORT, () => {
+            console.log("Server Started");
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
--- src/models/models.ts
+++ src/models/models.ts
@@ -2,242 +2,356 @@ import bcrypt from "bcrypt";
 import { Document, Schema, Types, model } from "mongoose";
 
 export interface UserDocument extends Document {
-  _id: string;
-  userName: string;
-  email: string;
-  password: string;
-  profilePicture: string;
-  publicId: string;
-  expenses: [Types.ObjectId];
-  incomes: [Types.ObjectId];
-  totalBalance: number;
-  totalIncome: number;
-  totalExpense: number;
+    _id: string;
+    userName: string;
+    email: string;
+    password: string;
+    profilePicture: string;
+    publicId: string;
+    expenses: Types.ObjectId[];
+    incomes: Types.ObjectId[];
+    totalBalance: number;
+    totalIncome: number;
+    totalExpense: number;
+    groups: Types.ObjectId[];
 }
 
 export interface UserDataDocument extends Document {
-  _id: string;
-  email: string;
-  userName: string;
-  password: string;
-  profilePicture: string;
-  createdAt: Date;
+    _id: string;
+    email: string;
+    userName: string;
+    password: string;
+    profilePicture: string;
+    createdAt: Date;
 }
 
 export interface OTPDocument extends Document {
-  otp: string;
-  email: string;
-  createdAt: Date;
+    otp: string;
+    email: string;
+    createdAt: Date;
 }
 
 export interface TransactionDocument extends Document {
-  _id: string;
-  transactionAmount: string;
-  category: string;
-  transactionTitle: string;
-  notes: string;
-  invoiceUrl: string;
-  publicId: string;
-  transactionDate: string;
-  type: string;
-  createdBy: Types.ObjectId;
+    _id: string;
+    transactionAmount: string;
+    category: string;
+    transactionTitle: string;
+    notes: string;
+    invoiceUrl: string;
+    publicId: string;
+    transactionDate: string;
+    type: string;
+    createdBy: Types.ObjectId;
 }
 
 export interface MonthlyHistoryDocument extends Document {
-  _id: string;
-  user: Types.ObjectId;
-  month: string;
-  year: string;
-  transactionIds: [Types.ObjectId];
-  monthlyBalance: number;
-  income: number;
-  expense: number;
+    _id: string;
+    user: Types.ObjectId;
+    month: string;
+    year: string;
+    transactionIds: [Types.ObjectId];
+    monthlyBalance: number;
+    income: number;
+    expense: number;
 }
 
 const userSchema = new Schema<UserDocument>({
-  userName: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-  },
-  profilePicture: {
-    type: String,
-    contentType: String,
-  },
-  publicId: {
-    type: String,
-  },
-  expenses: {
-    type: [Types.ObjectId],
-    ref: "Expense",
-    required: true,
-  },
-  incomes: {
-    type: [Types.ObjectId],
-    ref: "Income",
-    required: true,
-  },
-  totalBalance: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-  totalIncome: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-  totalExpense: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+    },
+    profilePicture: {
+        type: String,
+        contentType: String,
+    },
+    publicId: {
+        type: String,
+    },
+    expenses: {
+        type: [Types.ObjectId],
+        ref: "Expense",
+        required: true,
+    },
+    incomes: {
+        type: [Types.ObjectId],
+        ref: "Income",
+        required: true,
+    },
+    totalBalance: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    totalIncome: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    totalExpense: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    groups: {
+        type: [Types.ObjectId],
+        ref: "Group",
+        required: true,
+        default: [],
+    },
 });
 
 userSchema.pre<UserDocument>("save", async function (next) {
-  const user = this;
-  if (!user.isModified("password")) {
-    return next();
-  }
+    const user = this;
+    if (!user.isModified("password")) {
+        return next();
+    }
 
-  try {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(user.password, salt);
-    user.password = hashedPassword;
-    next();
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(user.password, salt);
+        user.password = hashedPassword;
+        next();
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 export const User = model("User", userSchema);
 
 const dataSchema = new Schema<UserDataDocument>({
-  userName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-  },
-  profilePicture: {
-    type: String,
-    contentType: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    expires: 600,
-  },
+    userName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+    },
+    profilePicture: {
+        type: String,
+        contentType: String,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 600,
+    },
 });
 
 export const UserData = model<UserDataDocument>("UserData", dataSchema);
 
 const otpSchema = new Schema<OTPDocument>({
-  otp: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    expires: 600,
-  },
+    otp: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 600,
+    },
 });
 
 export const OTP = model<OTPDocument>("OTP", otpSchema);
 
 const transactionSchema = new Schema<TransactionDocument>({
-  transactionAmount: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  transactionTitle: {
-    type: String,
-    required: true,
-  },
-  notes: {
-    type: String,
-  },
-  invoiceUrl: {
-    type: String,
-  },
-  publicId: {
-    type: String,
-  },
-  transactionDate: {
-    type: String,
-    required: true,
-  },
-  type: {
-    type: String,
-    required: true,
-  },
-  createdBy: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
+    transactionAmount: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    transactionTitle: {
+        type: String,
+        required: true,
+    },
+    notes: {
+        type: String,
+    },
+    invoiceUrl: {
+        type: String,
+    },
+    publicId: {
+        type: String,
+    },
+    transactionDate: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
 });
 
 export const Transaction = model<TransactionDocument>(
-  "Transaction",
-  transactionSchema
+    "Transaction",
+    transactionSchema
 );
 
 const monthlyHistorySchema = new Schema<MonthlyHistoryDocument>({
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  month: {
-    type: String,
-    required: true,
-  },
-  year: {
-    type: String,
-    required: true,
-  },
-  transactionIds: {
-    type: [Schema.Types.ObjectId],
-    required: true,
-  },
-  monthlyBalance: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-  income: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
-  expense: {
-    type: Number,
-    required: true,
-    default: 0,
-  },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    month: {
+        type: String,
+        required: true,
+    },
+    year: {
+        type: String,
+        required: true,
+    },
+    transactionIds: {
+        type: [Schema.Types.ObjectId],
+        required: true,
+    },
+    monthlyBalance: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    income: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    expense: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
 });
 
 export const History = model("History", monthlyHistorySchema);
+
+export interface GroupTransactionDocument extends Document {
+    _id: string;
+    groupId: Types.ObjectId;
+    paidBy: Types.ObjectId;
+    splitAmong: Types.ObjectId[];
+    category: string;
+    transactionTitle: string;
+    notes: string;
+    invoiceUrl: string;
+    publicId: string;
+    transactionDate: string;
+}
+
+const groupTransactionSchema = new Schema<GroupTransactionDocument>({
+    groupId: {
+        type: Schema.Types.ObjectId,
+        ref: "Group",
+        required: true,
+    },
+    paidBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    splitAmong: {
+        type: [Schema.Types.ObjectId],
+        ref: "User",
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    transactionTitle: {
+        type: String,
+        required: true,
+    },
+    notes: {
+        type: String,
+    },
+    invoiceUrl: {
+        type: String,
+    },
+    publicId: {
+        type: String,
+    },
+    transactionDate: {
+        type: String,
+        required: true,
+    },
+});
+
+export const GroupTransaction = model(
+    "GroupTransaction",
+    groupTransactionSchema
+);
+
+export interface GroupDocument extends Document {
+    groupName: string;
+    groupProfile: string;
+    publicId: string;
+    createdBy: Types.ObjectId;
+    members: Types.ObjectId[];
+    groupExpense: Types.ObjectId[];
+    totalExpense: number;
+    category: string;
+}
+
+const groupSchema = new Schema<GroupDocument>({
+    groupName: {
+        type: String,
+        required: true,
+    },
+    groupProfile: {
+        type: String,
+    },
+    publicId: {
+        type: String,
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    members: {
+        type: [Schema.Types.ObjectId],
+        ref: "User",
+        default: [],
+    },
+    groupExpense: {
+        type: [Schema.Types.ObjectId],
+        ref: "GroupTransaction",
+        default: [],
+    },
+    totalExpense: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    category: {
+        type: String,
+        default: "NONE",
+    },
+});
+
+export const Group = model<GroupDocument>("Group", groupSchema);
