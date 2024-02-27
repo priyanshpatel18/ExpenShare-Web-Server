d5cf3ec9511993d377afea733d474982b0ac7756
Mr. V <vaidikbhesaniya1212@gmail.com>
2024-02-27 13:44:59 +0530

    getallgroupcontroller

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,64 @@ import cors from "cors";
 import "dotenv/config";
 import express, { Express } from "express";
 import { MongoClient } from "mongodb";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import mongoose from "mongoose";
 import cron from "node-cron";
 import path from "path";
 // File Imports
+import * as socketController from "./controllers/socketControllers";
 import { userRouter, transactionRouter, groupRouter } from "./routes/router";
 
 // Creating Backend Application
 const app: Express = express();
+// Create a HTTP Server
+const server = createServer(app);
+// Create an IO Server
+export const io = new Server(server);
+// Socket Connection
+
+// Create an interface to define the email-to-socket mapping
+interface EmailToSocketMap {
+    [email: string]: string;
+}
+
+// Create an empty object to store the mapping between email and socket ID
+export const emailToSocketMap: EmailToSocketMap = {};
+
+io.on("connection", (socket) => {
+    console.log("User", socket.id);
+
+    // Bind email to the socketId
+    socket.on("login", (email) => {
+        if (email) {
+            emailToSocketMap[email] = socket.id;
+            console.log(emailToSocketMap);
+        } else {
+            console.log("Invalid email:", email);
+        }
+    });
+
+    // Filter Users
+    socket.on("getUsers", (filter: string) => {
+        socketController.handleGetUsers(socket, filter);
+    });
+    // Sends Request
+    socket.on("sendRequest", (data) => {
+        socketController.handleSendRequest(socket, data);
+    });
+
+    socket.on("disconnect", () => {
+        socket.disconnect();
+        console.log("Socket disconnected", socket.id);
+    });
+});
 
 // Middlewares
 app.use(
     cors({
         origin: "https://expenshare.vercel.app",
+      
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
